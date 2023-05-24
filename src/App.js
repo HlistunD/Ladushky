b8589ec1b04cfe9ee3bcbaa34999f53b6ae8dd24
Dasha Khlistun <dashakhlistun@macbook-air.myfiosgateway.com>
2023-05-24 17:30:37 -0400

    message

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import React from "react";
 import Navigation from "./Navigation";
+import Ladushky from "./Ladushky";
 
 export default function App() {
   return (
     <div>
         <Navigation/>
+        <Ladushky/>
       </div>
   )
 }
