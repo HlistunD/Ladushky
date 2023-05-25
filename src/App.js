930cf9488cd2c75b62705f17bd3450ccb0d36f1c
Dasha Khlistun <dashakhlistun@macbook-air.myfiosgateway.com>
2023-05-24 21:10:12 -0400

    message

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import "./App.css";
 import React from "react";
-import Navigation from "./Navigation";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Ladushky from "./Ladushky";
+import Blankets from "./Blankets";
+import Shop from "./Shop";
+import Contact from "./Contact";
 
 export default function App() {
-  return (
-    <div>
-      <Navigation />
-    </div>
-  )
+  return <Router>
+      <nav>
+          <Link to="/" className="link">Ladushky Story</Link>
+          <Link to="/blankets" className="link">What we use</Link>
+          <Link to="/shop" className="link">Shop</Link>
+          <Link to="/contacts" className="link">Our contact</Link>
+      </nav>
+      <Routes>
+          <Route path="/" element={<Ladushky />} />
+          <Route path="/blankets" element={<Blankets />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/contacts" element={<Contact />} />
+          <Route path="*" element={<Ladushky />} />
+      </Routes>
+  </Router>
 }
